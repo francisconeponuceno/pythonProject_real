df12e035d9785023a117210b830b7ffc5dc58793
FRANCISCONEPONUCENO <franciscocan1986@gmail.com>
2023-04-18 13:12:31 -0300

    Initial commit

---
+++ GERENCIAMENTO.py
@@ -0,0 +1,33 @@
+print('olá mundo!')
+from barcode import EAN13
+from  barcode.writer import ImageWriter
+from datetime import datetime
+from datetime import date,timedelta
+import reportlab
+import PyQt5
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import SimpleDocTemplate, Image
+from reportlab.lib import colors
+import webbrowser
+import os
+from random import randint
+from PyQt5 import uic,QtWidgets
+from PyQt5.QtWidgets import *
+import sqlite3
+from time import sleep
+from num2words import num2words
+from PyQt5.QtCore import QPropertyAnimation,QEasingCurve
+from PyQt5 import QtCore,QtWidgets
+import requests
+import json
+#import auto_py_to_exe
+import sys
+
+
+app=QtWidgets.QApplication([])
+sistema = uic.loadUi('progeto_real.ui')
+sistema.show()
+app.exec()
